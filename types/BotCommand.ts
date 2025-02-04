1ab0875591bdc10ecadad70e031e05687f9bbea4
afkbeto <qv.nguyen00@gmail.com>
2025-02-04 13:54:40 +0100

    feat: add ContextMenuCommand
    split BotCommand into SlashCommand and ContextMenu

--- types/BotCommand.ts
+++ types/BotCommand.ts
@@ -1,21 +1,34 @@
 import {
     CommandInteraction,
+    ContextMenuCommandBuilder,
+    MessageContextMenuCommandInteraction,
     SlashCommandBuilder,
-    SlashCommandOptionsOnlyBuilder,
-    SlashCommandSubcommandsOnlyBuilder,
 } from 'discord.js';
 
 /**
- * The ICommand type.
+ * The SlashCommand type.
  * @property {number} [cooldown] - The cooldown of the command.
  * @property {SlashCommandBuilder | SlashCommandSubcommandsOnlyBuilder | SlashCommandOptionsOnlyBuilder} data - The data of the command.
  * @property {Function} execute - The function to execute when the command is called.
  */
-export type BotCommand = {
+export type SlashCommand = {
     cooldown?: number;
-    data:
-        | SlashCommandBuilder
-        | SlashCommandSubcommandsOnlyBuilder
-        | SlashCommandOptionsOnlyBuilder;
+    data: SlashCommandBuilder;
     execute: (interaction: CommandInteraction) => Promise<void>;
 };
+
+/**
+ * The ContextMenuCommand type.
+ * @property {number} [cooldown] - The cooldown of the command.
+ * @property {ContextMenuCommandBuilder} data - The data of the command.
+ * @property {Function} execute - The function to execute when the command is called.
+ */
+export type ContextMenuCommand = {
+    cooldown?: number;
+    data: ContextMenuCommandBuilder;
+    execute: (
+        interaction: MessageContextMenuCommandInteraction,
+    ) => Promise<void>;
+};
+
+export type BotCommand = SlashCommand | ContextMenuCommand;
--- utils/typeguard.ts
+++ utils/typeguard.ts
@@ -1,4 +1,7 @@
+import { SlashCommandBuilder } from 'discord.js';
 import { BotCommand } from '../types/_index.ts';
+import { ContextMenuCommand, SlashCommand } from '../types/BotCommand.ts';
+import { ContextMenuCommandBuilder } from 'discord.js';
 
 /**
  * Type guard for ICommand
@@ -8,4 +11,14 @@ import { BotCommand } from '../types/_index.ts';
 // deno-lint-ignore no-explicit-any -- This is a type guard, so it needs to be any
 export function isBotCommand(object: any): object is BotCommand {
     return 'data' in object && 'execute' in object;
+}
+
+export function isSlashCommand(command: BotCommand): command is SlashCommand {
+    return command.data instanceof SlashCommandBuilder;
+}
+
+export function isContextMenuCommand(
+    command: BotCommand,
+): command is ContextMenuCommand {
+    return command.data instanceof ContextMenuCommandBuilder;
 }
