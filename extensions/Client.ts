8cdbbf9c30fd18c7489034e63540ee31fcf4e1df
QV Nguyen <91276801+AFKBETO@users.noreply.github.com>
2025-03-21 11:22:30 +0100

    feat: add command collections in client

--- extensions/Client.ts
+++ extensions/Client.ts
@@ -1,7 +1,15 @@
 import { Collection } from 'discord.js';
+import {
+    MessageContextMenuCommand,
+    SlashCommand,
+    UserContextMenuCommand,
+} from '../types/_index.ts';
 
 declare module 'discord.js' {
     interface Client {
         cooldowns: Collection<string, Collection<string, number>>;
+        slashCommands: Collection<string, SlashCommand>;
+        messageContextCommands: Collection<string, MessageContextMenuCommand>;
+        userContextCommands: Collection<string, UserContextMenuCommand>;
     }
 }
