c415040be55b2dfa3592c4dc58e4ec589f97a480
QV Nguyen <91276801+AFKBETO@users.noreply.github.com>
2025-04-10 15:07:35 +0200

    feat: decouple the logic from data

--- database/modules/matchData.ts
+++ database/modules/matchData.ts
@@ -1,7 +1,5 @@
 import MatchModel, { MatchInt } from '../models/MatchModel.ts';
 
-const EXP_TIME = 86400;
-
 export async function getMatchData(
     id1: string,
     id2: string,
@@ -18,12 +16,6 @@ export async function getMatchData(
             value: 0,
             timestamp: 0,
         });
-
-    if ((Date.now() - matchData.timestamp) / 1000 > EXP_TIME) {
-        matchData.value = Math.floor(Math.random() * 101);
-        matchData.timestamp = Date.now();
-        await matchData.save();
-    }
 
     return matchData;
 }
