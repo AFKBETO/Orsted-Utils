53489fb9cf6235840034f80f78de0fe3a5fedb45
QV Nguyen <91276801+AFKBETO@users.noreply.github.com>
2025-05-16 22:25:14 +0200

    refactor: regroup all command collections into one

--- extensions/Client.ts
+++ extensions/Client.ts
@@ -1,18 +1,12 @@
 import { Collection } from 'discord.js';
-import {
-    MessageContextMenuCommand,
-    SlashCommand,
-    UserContextMenuCommand,
-} from '../types/_index.ts';
+import { BotCommand } from '../types/_index.ts';
 import { ConfigData } from '../database/models/ConfigModel.ts';
 import { RandomName } from '../utils/_index.ts';
 
 declare module 'discord.js' {
     interface Client {
         cooldowns: Collection<string, Collection<string, number>>;
-        slashCommands: Collection<string, SlashCommand>;
-        messageContextCommands: Collection<string, MessageContextMenuCommand>;
-        userContextCommands: Collection<string, UserContextMenuCommand>;
+        commands: Collection<string, BotCommand>;
         images: Collection<string, string[]>;
         botConfig: ConfigData;
         randomName: RandomName;
